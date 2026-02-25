051dd97ec3172008a1b7671d6294b36102f80591
chmielciuu <kamilchmielewski665@gmail.com>
2026-02-25 04:36:09 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: "Kamil Chmielewski Transport",
+  description: "Premium Passenger Transport in Wrocław",
+};
 
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
